68333f3eb454d6e22888eabb3e6f1346c017a30a
milan9527 <milan9527@hotmail.com>
2018-11-03 19:43:49 +0800

    Update S3CopyToChina-Monitor.py

--- S3CopyToChina-Monitor.py
+++ S3CopyToChina-Monitor.py
@@ -4,38 +4,64 @@ from boto3.dynamodb.conditions import Key, Attr
 
 def lambda_handler(event, context):
     ddb = boto3.resource('dynamodb')
-    
+
+    # Check if s3 multipart upload id exists.
+    uploadids = []
+    table = ddb.Table('S3MPUResult')
+    response = table.scan(
+        FilterExpression=Attr('complete').eq('N')
+        )
+    j = 0
+    for i in response['Items']:
+        info = response['Items'][j]
+        uploadid = info['uploadid']
+        uploadids.append(uploadid)
+        j += 1
+
     # Monitor S3 multi part upload task.
     table = ddb.Table('S3MPU')
     response = table.scan(
         FilterExpression=Attr('part_complete').eq('N')
         )
     j = 0
+    k = 0
     for i in response['Items']:
-        bucket = response['Items'][j]['source_bucket']
-        key = response['Items'][j]['source_key']
-        dst_bucket = response['Items'][j]['destination_bucket']
-        uploadid = response['Items'][j]['uploadid']
-        part = response['Items'][j]['part']
-        range = 'bytes='+response['Items'][j]['range']
-        event_str = {
-            'bucket' : bucket,
-            'key' : key,
-            'dst_bucket' : dst_bucket,
-            'uploadid' : uploadid,
-            'part' : str(part),
-            'range' : range
-        }
-        payload_json = json.dumps(event_str)
-        lambdaclient = boto3.client('lambda')
-        lambdaclient.invoke(
-            FunctionName='S3CopyToChina-MPU',
-            InvocationType='Event',
-            Payload=payload_json
-            )
+        info = response['Items'][j]
+        uploadid = info['uploadid']
+        bucket = info['source_bucket']
+        key = info['source_key']
+        dst_bucket = info['destination_bucket']
+        part = info['part']
+        range = 'bytes='+info['range']
+
+        if uploadid in uploadids:
+            event_str = {
+                'bucket' : bucket,
+                'key' : key,
+                'dst_bucket' : dst_bucket,
+                'uploadid' : uploadid,
+                'part' : str(part),
+                'range' : range
+            }
+            payload_json = json.dumps(event_str)
+            lambdaclient = boto3.client('lambda')
+            lambdaclient.invoke(
+                FunctionName='S3CopyToChina-MPU',
+                InvocationType='Event',
+                Payload=payload_json
+                )
+            k += 1
+        else:
+            table.delete_item(
+                    Key={
+                        'uploadid': uploadid,
+                        'part': part
+                    }
+                )
         j += 1
-    print('Invoke '+str(j)+' Lambda to restart timeout tasks.')
-    
+        
+    print('Invoke '+str(k)+' Lambda to restart timeout tasks for multi-parts object.')
+        
     # Monitor S3 single object task.
     table = ddb.Table('S3Single')
     response = table.scan(
@@ -43,8 +69,9 @@ def lambda_handler(event, context):
         )
     j = 0
     for i in response['Items']:
-        bucket = response['Items'][j]['source_bucket']
-        key = response['Items'][j]['key']
+        info = response['Items'][j]
+        bucket = info['source_bucket']
+        key = info['key']
         event_str = {
                     	'Records': [{
                     	    "eventName": "ObjectCreated:Put",
@@ -66,7 +93,4 @@ def lambda_handler(event, context):
             Payload=payload_json
             )
         j += 1
-    print('Invoke '+str(j)+' Lambda to restart timeout tasks.')
-
-
-    
+    print('Invoke '+str(j)+' Lambda to restart timeout tasks for single object.')
